e8e639fbbdcf0e66d928f500ae373c9a80d93251
AliceNg429 <wuzhenni429@qq.com>
2019-08-17 17:01:16 +0800

    基于类型安全的配置方式

--- src/main/java/com/wisely/springboot/SpringbootApplication.java
+++ src/main/java/com/wisely/springboot/SpringbootApplication.java
@@ -1,10 +1,22 @@
 package com.wisely.springboot;
 
+import com.wisely.springboot.config.AuthorSettings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringbootApplication {
+    @Autowired
+    private AuthorSettings authorSettings; //1
+
+    @RequestMapping("/")
+    public String index() {
+        return "author name is " + authorSettings.getName() + " and author age is " + authorSettings.getAge();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
